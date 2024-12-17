888dbf9907ae66da3937c862b010d02d83dc84c9
juanmira19 <jmirac@unal.edu.co>
2024-12-17 11:06:39 -0500

    6

--- zooAnimales/pez.py
+++ zooAnimales/pez.py
@@ -1,22 +1,45 @@
-from zooAnimales.animal import Animal
-
+from .animal import Animal
 class Pez(Animal):
-
-    cantidadPeces=0
-    listado=[]
+    _listado=[]
     salmones=0
     bacalaos=0
-    def __init__(self,nombre=None,edad=None,habitat=None,genero=None,colorEscamas=None,cantidadAletas=0,):
+    def __init__(self,nombre=None,edad=0,habitat=None,genero=None,colorEscamas=None,cantidadAletas=0):
         super().__init__(nombre,edad,habitat,genero)
-        self.colorEscamas=colorEscamas
-        self.cantidadAletas=cantidadAletas
-        Pez.cantidadPeces+=1
-
-    def getNombre(self):
-        return self.nombre
-    
-    def setNombre(self,nombre):
-        self.nombre=nombre
-
-    def movimiento():
+        self._colorEscamas=colorEscamas
+        self._cantidadAletas=cantidadAletas
+        Pez._listado.append(self)
+    @classmethod 
+    def cantidadPeces(cls):
+        return len(cls._listado)
+    def movimiento(self):
         return "nadar"
+    @classmethod
+    def crearSalmon(cls,x,y,z):
+        a=Pez(x, y, "oceano", z, "rojo", 6)
+        cls.salmones+=1
+        return a
+    @classmethod
+    def crearBacalao(cls,x,y,z):
+        a=Pez(x, y, "oceano", z, "gris", 6)
+        cls.bacalaos+=1
+        return a
+    
+    @classmethod
+    def setListado(cls,l):
+        cls._listado=l
+        
+    @classmethod
+    def getListado(cls):
+        return cls._listado
+    
+    def setColorEscamas(self,c):
+        self._colorEscamas=c
+        
+    def getColorEscamas(self):
+        return self._colorEscamas
+    
+    def setCantidadAletas(self,p):
+        self._cantidadAletas=p
+        
+    def getCantidadAletas(self):
+        return self._cantidadAletas
--- zooAnimales/reptil.py
+++ zooAnimales/reptil.py
@@ -1,43 +1,45 @@
 from .animal import Animal
-
 class Reptil(Animal):
-
-    cantidadReptiles=0
-    listado=[]
+    _listado=[]
     iguanas=0
     serpientes=0
-    def __init__(self,nombre=None,edad=None,habitat=None,genero=None,colorEscamas=None,largoCola=0,):
+    def __init__(self,nombre=None,edad=0,habitat=None,genero=None,colorEscamas=None,largoCola=0):
         super().__init__(nombre,edad,habitat,genero)
-        self.colorEscamas=colorEscamas
-        self.largoCola=largoCola
-        Reptil.cantidadReptiles+=1
-
-    def getNombre(self):
-        return self.nombre
-    
-    def setNombre(self,nombre):
-        self.nombre=nombre
-
-    def movimiento():
+        self._colorEscamas=colorEscamas
+        self._largoCola=largoCola
+        Reptil._listado.append(self)
+    @classmethod 
+    def cantidadReptiles(cls):
+        return len(cls._listado)
+    def movimiento(self):
         return "reptar"
-    
-
     @classmethod
-    def crearIguana(cls, nombre, edad, genero):
-        
-        M=Reptil(nombre,edad,"humedal",genero,"blanco y amarillo",3)
+    def crearIguana(cls,x,y,z):
+        a=Reptil(x, y, "humedal", z, "verde", 3)
         cls.iguanas+=1
-                
-        return M
+        return a
+    @classmethod
+    def crearSerpiente(cls,x,y,z):
+        a=Reptil(x,y,"jungla",z,"blanco",1)
+        cls.serpientes+=1
+        return a
     
     @classmethod
-    def crearSerpiente(cls, nombre, edad, genero):
+    def setListado(cls,l):
+        cls._listado=l
         
-        M=Reptil(nombre,edad,"jungla",genero,"blanco",1)
-        cls.serpientes+=1
-                
-        return M
-
-
-
-
+    @classmethod
+    def getListado(cls):
+        return cls._listado
+    
+    def setColorEscamas(self,c):
+        self._colorEscamas=c
+        
+    def getColorEscamas(self):
+        return self._colorEscamas
+    
+    def setLargoCola(self,p):
+        self._largoCola=p
+        
+    def getLargoCola(self):
+        return self._largoCola
