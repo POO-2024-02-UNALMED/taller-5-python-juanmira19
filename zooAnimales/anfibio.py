4e93253974e438044faa54f216c35ff43911759a
juanmira19 <jmirac@unal.edu.co>
2024-12-17 11:04:22 -0500

    Update anfibio.py

--- zooAnimales/anfibio.py
+++ zooAnimales/anfibio.py
@@ -1,27 +1,47 @@
 from zooAnimales.animal import Animal
-
 class Anfibio(Animal):
-
-    cantidadAnfibios=0
+    _listado=[]
     ranas=0
     salamandras=0
-    listado=[]
-
-    def __init__(self,nombre=None,edad=None,habitat=None,genero=None,colorPiel=None,venenoso=False):
+    def __init__(self,nombre=None,edad=0,habitat=None,genero=None,colorPiel=None,venenoso=False):
         super().__init__(nombre,edad,habitat,genero)
-        self.colorPiel=colorPiel
-        self.venenoso=venenoso
-        Anfibio.cantidadAnfibios+=1
-
-
-
-    def getNombre(self):
-        return self.nombre
+        self._colorPiel=colorPiel
+        self._venenoso=venenoso
+        Anfibio._listado.append(self)
+    @classmethod 
+    def cantidadAnfibios(cls):
+        return len(cls._listado)
     
-    def setNombre(self,nombre):
-        self.nombre=nombre
-
-    def movimiento():
+    def movimiento(self):
         return "saltar"
     
-
+    @classmethod
+    def crearRana(cls,x,y,z):
+        a=Anfibio(x, y, "selva", z, "rojo", True)
+        cls.ranas+=1
+        return a
+    @classmethod
+    def crearSalamandra(cls,x,y,z):
+        a=Anfibio(x, y, "selva", z, "negro", False)
+        cls.salamandras+=1
+        return a
+    
+    @classmethod
+    def setListado(cls,l):
+        cls._listado=l
+        
+    @classmethod
+    def getListado(cls):
+        return cls._listado
+    
+    def setColorPiel(self,c):
+        self._colorPiel=c
+        
+    def getColorPiel(self):
+        return self._colorPiel
+    
+    def setVenenoso(self,v):
+        self._venenoso=v
+        
+    def isVenenoso(self):
+        return self._venenoso
