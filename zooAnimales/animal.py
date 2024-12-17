a4a6ad01e648b8ff8149405dfd79581c641654d4
juanmira19 <jmirac@unal.edu.co>
2024-12-17 11:05:14 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,16 +1,17 @@
 
-
-class Animal():
-    totalAnimales=0
-    def __init__ (self,nombre=None,edad=None,habitat=None,genero=None,zona=None):
+class Animal:
+    _totalAnimales=0
+    def __init__(self,nombre=None,edad=0,habitat=None,genero=None):
+        Animal._totalAnimales+=1
         self.nombre=nombre
         self.edad=edad
         self.habitat=habitat
         self.genero=genero
-        self.zona=zona
+        self.zona=None
+        
+    def movimiento(self):
+        return"desplazarse"
     
-    def movimiento():
-        return "desplazarse"
     @classmethod
     def totalPorTipo(cls):
         from zooAnimales.anfibio import Anfibio
@@ -19,11 +20,45 @@ class Animal():
         from zooAnimales.pez import Pez
         from zooAnimales.reptil import Reptil
         return f"Mamiferos : {Mamifero.cantidadMamiferos()}\nAves : {Ave.cantidadAves()}\nReptiles : {Reptil.cantidadReptiles()}\nPeces : {Pez.cantidadPeces()}\nAnfibios : {Anfibio.cantidadAnfibios()}"
+    
+    def __str__(self):
+        return f"Mi nombre es {self.nombre}, tengo una edad de {self.edad}, habito en {self.habitat} y mi genero es {self.genero}"
+    
+    @classmethod
+    def setTotalAnimales(cls,t):
+        cls._totalAnimales=t
+        
+    @classmethod
+    def getTotalAnimales(cls):
+        return cls._totalAnimales
+    
+    def setNombre(self,n):
+        self.nombre=n
+        
     def getNombre(self):
         return self.nombre
     
-    def setNombre(self,nombre):
-        self.nombre=nombre
-
-
+    def setHabitat(self,h):
+        self.habitat=h
+        
+    def getHabitat(self):
+        return self.habitat
     
+    def setGenero(self,g):
+        self.genero=g
+    
+    def getGenero(self):
+        return self.genero
+    
+    def setZona(self, z):
+        self.zona=z
+        
+    def getZona(self):
+        return self.zona
+    
+    def getEdad(self):
+        return self.edad
+    
+    def setEdad(self, e):
+        self.edad=e
+
